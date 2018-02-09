903a13d87ad12e20841a9c817820ede94bd852e0
Ole Stubben <2270838s@student.gla.ac.uk>
2018-02-09 18:15:15 +0000

    End of chapter 8

--- rango/forms.py
+++ rango/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from rango.models import Page, Category
 
+NAME_TITLE = 128
+
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128,
+    name = forms.CharField(max_length=NAME_TITLE,
                            help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial = 0)
@@ -13,7 +15,7 @@ class CategoryForm(forms.ModelForm):
         fields = ('name',)
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128,
+    title = forms.CharField(max_length=NAME_TITLE,
                             help_text="Please enter the title of the page.")
     url = forms.CharField(max_length=200,
                           help_text="Please enter the URL of the page.")
@@ -23,3 +25,13 @@ class PageForm(forms.ModelForm):
         model = Page
 
         exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+            return cleaned_data
--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
-from rango.forms import CategoryForm
+from rango.forms import CategoryForm,PageForm
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
@@ -27,8 +27,8 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = pages
         context_dict['category'] = category
     except Category.DoesNotExist:
-        context_dict['pages'] = None
         context_dict['category'] = None
+        context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
 
@@ -45,3 +45,25 @@ def add_category(request):
             print(form.errors)
 
     return render(request, 'rango/add_category.html', {'form' : form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    form = PageForm()
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if category:
+                form.save(commit=True)
+                page.category = category
+                page.views = 0
+                page.save()
+                return index(request, category_name_slug)
+        else:
+            print(form.errors)
+
+    context_dict = {'form' : form, 'category' : category }
+    return render(request, 'rango/add_page.html', {'form' : form})
